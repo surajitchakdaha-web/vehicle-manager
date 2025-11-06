223556317b3646d4ae38b1f9f8ce3f17df2ffd12
surajitchakdaha-web <surajit.chakdaha@gmail.com>
2025-11-06 17:57:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,46 @@
-﻿self.addEventListener("install", event => {
+const CACHE_NAME = "vehicle-manager-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("vehicle-manager-cache-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./FuelTracker.png",
-        "./vehicle.html"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener("fetch", event => {
+// Fetch Cached Files
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Update Cache if Needed
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
